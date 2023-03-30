77bbc22ef113c8b1d7dc96a62afd98504f64e87e
VictorMNSalas <victorsalasnz@gmail.com>
2023-03-30 10:34:33 -0600

    inicios de vue

---
+++ VUE/myapp/src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>Counter</h2>
+        <p>{{ num }}</p>
+        <button @click="aumentar">Ingrementar</button>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+    name: 'Counter',
+    data() {
+        return {
+            num: 0
+        }
+    },
+    methods: {
+        aumentar() {
+            this.num = ref(this.num)
+            this.num = this.num + 1
+        }
+    }
+}
+</script>
+
+<style></style>
