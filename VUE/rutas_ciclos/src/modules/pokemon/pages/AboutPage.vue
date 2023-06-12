4e19438135b29e6bc397c4460bb9c76956e244a0
Víctor Manuel Nuñez Sandra <victorsalasnz@gmail.com>
2023-06-12 15:49:21 -0600

    Lifecycle

--- VUE/rutas_ciclos/src/modules/pokemon/pages/AboutPage.vue
+++ VUE/rutas_ciclos/src/modules/pokemon/pages/AboutPage.vue
@@ -4,10 +4,41 @@
 
 <script>
 export default {
-
-}
+  beforeCreate() {
+    console.log('______________________________Ciclos de Vida de VUE______________________________')
+    console.log("beforeCreate")
+  },
+  created() {
+    console.log("Created")
+  },
+  beforeMount() {
+    console.log("beforeMount")
+  },
+  updated() {
+    console.log("update")
+  },
+  activated() {
+    console.log("activated")
+  },
+  deactivated() {
+    console.log("deactivated")
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount")
+  },
+  unmounted() {
+    console.log("unmounted")
+  },
+  errorCaptured() {
+    console.log("errorCaptured")
+  },
+  renderTracked() {
+    console.log("renderTracker")
+  },
+  renderTriggered() {
+    console.log("renderTriggered")
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- VUE/rutas_ciclos/src/modules/pokemon/pages/ListPage.vue
+++ VUE/rutas_ciclos/src/modules/pokemon/pages/ListPage.vue
@@ -1,13 +1,11 @@
 <template>
   <h1>List Page</h1>
+
 </template>
 
 <script>
 export default {
-
-}
+ };
 </script>
 
-<style>
-
-</style>
+<style></style>
