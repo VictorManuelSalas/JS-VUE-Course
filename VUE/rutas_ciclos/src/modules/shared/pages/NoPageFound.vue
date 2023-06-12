c10766e1208fc94c30c3a7463cec3354366a6dc0
Víctor Manuel Nuñez Sandra <victorsalasnz@gmail.com>
2023-06-12 11:56:43 -0600

    routes pages path

---
+++ VUE/rutas_ciclos/src/modules/shared/pages/NoPageFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1><b>Page not found!</b> <br> 404</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ VUE/rutas_ciclos/src/routes/router.js
@@ -0,0 +1,20 @@
+import AboutPage from "../modules/pokemon/pages/AboutPage";
+import ListPage from "../modules/pokemon/pages/ListPage";
+import PokemonPage from "../modules/pokemon/pages/PokemonPage";
+import NoPageFound from "../modules/shared/pages/NoPageFound"
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const routes = [
+  { path: "/", component: ListPage },
+  { path: "/about", component: AboutPage },
+  { path: "/id", component: PokemonPage },
+  { path: "/:pathMatch(.*)*", component: NoPageFound },
+];
+
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`
+});
+
+export default router;
